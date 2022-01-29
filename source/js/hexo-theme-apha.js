d5efdc15873f18b3724087f183047bf21561b992
JonnyJong <jonnyjong@outlook.com>
2022-01-29 19:04:59 +0800

    add：完整的顶栏功能

--- source/js/hexo-theme-apha.js
+++ source/js/hexo-theme-apha.js
@@ -2,23 +2,35 @@
 window.onscroll = function(e){
   if (saveOffset == 'none') {
     saveOffset = window.pageYOffset;
-  }
-  if (jsC_navFold == 'none') {
     jsC_navFold = document.getElementById("jsConfig").classList.contains("jsC_navFold") ? true : false;
   }
-  navbar = document.getElementById("navbar");
+  pcNavbar = document.getElementById("pcNavbar");
   var deltaT = saveOffset - window.pageYOffset;
   if (jsC_navFold && deltaT < 0) {
-    navbar.className = "navbar nav_hide";
+    pcNavbar.className = "navbar nav_hide";
   } else if (jsC_navFold && deltaT > 0) {
-    navbar.className = "navbar";
+    pcNavbar.className = "navbar";
   }
   saveOffset = window.pageYOffset;
 }
 
-var saveOffset = 'none';
-var navbar;
-var jsC_navFold = 'none';
+var saveOffset = 'none', pcNavbar, jsC_navFold;
+
+// 手机端打开菜单
+function nav_menu_btn() {
+  if (mblNavbar == "none"){
+    mblNavbar = document.getElementById("mblNavbar");
+    mblNavBakCover = document.getElementById("nav_menu_cover");
+  }
+  if (mblNavbar.className == "navbar"){
+    mblNavbar.className = "navbar show";
+    mblNavBakCover.className = "nav_menu_cover show";
+  }else{
+    mblNavbar.className = "navbar";
+    mblNavBakCover.className = "nav_menu_cover";
+  }
+}
+var mblNavbar = 'none', mblNavBakCover;
 
 // 随机色相
 /* window.onload = function() {
