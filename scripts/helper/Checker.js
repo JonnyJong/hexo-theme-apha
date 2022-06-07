3714999db5c03b550bea0e4dde106f8b355ab964
JonnyJong <jiangling2004@outlook.com>
2022-06-07 15:44:29 +0800

    首页横幅、其他细节调整

--- scripts/helper/Checker.js
+++ scripts/helper/Checker.js
@@ -3,3 +3,6 @@ hexo.extend.helper.register('Checker', function(input){
   console.log({type:typeof(input),input});
   return
 })
+hexo.extend.helper.register('Compare', function(input, input2){
+  return input == input2
+})
