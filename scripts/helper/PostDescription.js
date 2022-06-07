b0a00d1abe498c29fc7cf23d9c06949cf3451a06
JonnyJong <jiangling2004@outlook.com>
2022-06-07 14:11:57 +0800

    简化设置、新增渲染描述、修复随机取图错误、其他细节调整

--- scripts/helper/PostDescription.js
+++ scripts/helper/PostDescription.js
@@ -1,14 +1,19 @@
 'use strict'
-hexo.extend.helper.register('PostDescription', function(post, priority, limit, defaultContent){
-  // let priority = hexo.theme.index_posts.description.priority
-  // let limit = (hexo.theme.index_posts.description.max_length_of_content ? hexo.theme.index_posts.description.max_length_of_content : 300)
-  // let defaultContent = hexo.theme.index_posts.description.max_length_of_content ? hexo.theme.index_posts.description.max_length_of_content : ''
+hexo.extend.helper.register('PostDescription', function(post){
+  let priority = hexo.theme.config.index_posts.description.priority
+  let limit = (hexo.theme.config.index_posts.description.max_length_of_content ? hexo.theme.index_posts.description.max_length_of_content : 300)
+  let defaultContent = hexo.theme.config.index_posts.description.max_length_of_content ? hexo.theme.index_posts.description.max_length_of_content : ''
+  let render = !!hexo.theme.config.index_posts.description.render_excerpt
   let result = ''
   select: for (let i = 0; i < priority.length; i++) {
     switch (priority[i]) {
       case 'excerpt':
         if (post.excerpt){
-          result = this.strip_html(post.excerpt)
+          if (render) {
+            result = hexo.render.renderSync({ text: post.excerpt, engine: 'markdown' })
+          }else{
+            result = this.strip_html(post.excerpt)
+          }
           break select
         }
         break
--- scripts/helper/RandomImage.js
+++ scripts/helper/RandomImage.js
@@ -1,6 +1,6 @@
 'use strict'
 hexo.extend.helper.register('RandomImage', function(data){
-  const defaultCover = hexo.theme.index_posts.default_covers
+  const defaultCover = hexo.theme.config.index_posts.default_covers
   let hash = 0
   data.split("").forEach(e => hash += e.charCodeAt())
   return defaultCover[hash % defaultCover.length]
